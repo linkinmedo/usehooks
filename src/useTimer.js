6115914fa6ec037ab0cf4bd20a9d94326aea6dcc
Mohammed Shammout <fdsmohammed@gmail.com>
2018-12-29 22:27:56 +0200

    fix useTimer

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,14 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export default function useTimer (initialTime) {
+export default function useTimer(initialTime) {
   const [time, setTime] = useState(initialTime);
 
   useEffect(() => {
     const interval = setInterval(() => {
+      if (time <= 0) {
+        clearInterval(interval);
+        return;
+      }
       setTime(time - 1);
-    }, 1000)
+    }, 1000);
     return () => clearInterval(interval);
-  }, [initialTime])
+  });
 
   return time;
 }
